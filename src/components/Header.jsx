21838e09fc4f6a14b4af1b2462d4aa592809b738
tarek3raby <ttarekarabyy@gmail.com>
2024-09-22 07:15:59 +0300

    creating Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-gradient-to-r  from-blue-300 to-purple-400 shadow-lg">
+      <div className="max-w-6xl mx-auto flex justify-between items-center p-3">
+        {/* Logo */}
+        <Link href="/" className="text-2xl font-extrabold groub cursor-pointer ">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600">
+          Auth
+        </span>
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-800 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600">
+          App
+        </span>
+        </Link>
+        {/* Add nav menu */}
+        <nav>
+          <ul className="flex gap-4"> 
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/sign-in">Sign in</Link>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
